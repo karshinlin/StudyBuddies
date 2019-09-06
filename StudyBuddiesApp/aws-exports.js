4c11d10f8e5cf2c37f1624365ec5d60bb6a3c586
Karan Achtani <Karan.achtani@gatech.edu>
2019-09-06 13:26:13 -0400

    adding more files

---
+++ StudyBuddiesApp/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+    identityPoolId: 'us-east-1:198b4e6c-54b4-4a58-84c4-558de89695aa',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_17p7tjq0V',
+    userPoolWebClientId: '5vr59bpjur826vgpqqi4h5ot1g'
+  }
+  
+  export default awsConfig;
